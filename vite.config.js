be533859d8cf55880ddb4b3012e89e57816bb720
derhaodh <derhaodh@gmail.com>
2024-07-21 17:26:50 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -8,20 +8,10 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      input: entryPoints("index.html", "projects/testimonials/index.html"),
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "projects/testimonials/index.html"),
+      },
     },
   },
-  base: "/",
 });
-
-function entryPoints(...paths) {
-  const entries = paths.map(parse).map((entry) => {
-    const { dir, base, name, ext } = entry;
-    const key = join(dir, name);
-    const path = resolve(__dirname, dir, base);
-    return [key, path];
-  });
-
-  const config = Object.fromEntries(entries);
-  return config;
-}
