7308271344f6536fbaa96c9d52eaa0430c959b48
derhaodh <derhaodh@gmail.com>
2024-07-21 18:32:07 +0800

    add mode for vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
 // vite.config.js
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
 
-export default defineConfig({
-  alias: {
-    "~": __dirname,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "testimonials/index.html"),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    // vite config
+    // define: {
+    //   __APP_ENV__: JSON.stringify(env.APP_ENV),
+    // },
+    alias: {
+      "~": __dirname,
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          nested: resolve(__dirname, "testimonials/index.html"),
+        },
       },
     },
-  },
-  base: "/projects/",
+    base: mode === "development" ? "/" : "/projects/",
+  };
 });
